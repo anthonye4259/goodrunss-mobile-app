5879fe9b6ee3e6b44cd28d5325dca698762bd86a
Anthony Edwards <anthonyedwards@Anthonys-MacBook-Air-18.local>
2025-11-28 02:22:29 -0500

    Add tsconfig.json with path aliases and update metro config

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,11 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+// Enable support for package exports
+config.resolver.unstable_enablePackageExports = true;
 
 module.exports = withNativeWind(config, { input: './global.css' });
 
