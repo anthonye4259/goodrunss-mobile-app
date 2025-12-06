17843207892669c11c7202c31d9c19710731ba80
Anthony Edwards <anthonyedwards@Anthonys-MacBook-Air-18.local>
2025-12-06 03:24:21 -0500

    Simplify metro.config.js
    - Removed unused path import
    - Removed unstable_enablePackageExports (may cause issues)
    - Consistent quote style

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,6 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
-const path = require('path');
+const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-// Enable support for package exports
-config.resolver.unstable_enablePackageExports = true;
-
-module.exports = withNativeWind(config, { input: './global.css' });
-
+module.exports = withNativeWind(config, { input: "./global.css" });
